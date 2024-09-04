fe879db29c347f89fbc992d3bc6cbfa29d73a507
kymkrt <wh40k@naver.com>
2024-09-04 14:24:51 +0900

    20240904_1424 추가테스트

--- src/t50_Exercise/T4Run.java
+++ src/t50_Exercise/T4Run.java
@@ -7,5 +7,7 @@ public class T4Run {
 		int res = t4.add(10,20);
 		
 		System.out.println("res : "+res);
+		System.out.println("수정버전");
+		
 	}
 }
---
+++ src/t50_Exercise/T5.java
@@ -0,0 +1,7 @@
+package t50_Exercise;
+
+public class T5 {
+public static void main(String[] args) {
+	System.out.println("고기뷔페 가고 깊다");
+}
+}
