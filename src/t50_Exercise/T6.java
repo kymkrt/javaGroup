7148eb6221362ac01fd2e4270f6a3597153e71ad
kymkrt <wh40k@naver.com>
2024-09-04 14:31:44 +0900

    20240904_1431 배고파

--- src/t50_Exercise/T6.java
+++ src/t50_Exercise/T6.java
@@ -3,5 +3,8 @@ package t50_Exercise;
 public class T6 {
 	public static void main(String[] args) {
 		System.out.println("물가가 너무 비싸");
+		System.out.println("물가가 너무 비싸");
+		System.out.println("물가가 너무 비싸");
+		
 	}
 }
